e5e791a95b1d71748394b739ee670408fd27e460
Арсений <Onsha.z@yandex.ru>
2020-08-24 13:13:08 +0300

    added: some more functions + styles adjusted

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -3,52 +3,19 @@ import Style from './styles/item.module.scss'
 
 const Item = ({data, available, handleSelect, selected}) => {
 
-  return (
-  //     <div className={Style.elem}>
-  //     <div id={data.id} onClick={handleSelect} className={!available
-  //       ? `${Style.item_container} ${Style.item_disabled}`
-  //       : selected
-  //         ? `${Style.item_container} ${Style.item_selected}`
-  //         : Style.item_container}>
-  //
-  //     <div className={Style.item_text_wrapper}>
-  //       <span className={Style.item_slogan}>
-  //         {data.slogan}
-  //       </span>
-  //       <span className={Style.item_title}>
-  //         {data.title}
-  //       </span>
-  //       <span className={Style.item_filling}>
-  //         {data.filling}
-  //       </span>
-  //       <span className={Style.item_offer}>
-  //         {data.offer}
-  //       </span>
-  //     </div>
-  //     <div className={Style.item_quant}>
-  //       <span className={Style.item_quant_numb}>
-  //         {data.weight}
-  //       </span>
-  //       <span className={Style.item_quant_unit}>
-  //         {data.unit}
-  //       </span>
-  //     </div>
-  //   </div>
-  //
-  //   <span className={!available ? Style.item_comment_sold : Style.item_comment}>
-  //     {
-  //       !available
-  //         ? data.soldOut :
-  //       !selected
-  //         ? data.callToAction
-  //         : data.tasteDetail
-  //     }
-  //   </span>
-  //
-  // </div>);
-  // }
+  const [isHovered, setHovered] = React.useState(false);
 
-  <div className={Style.item} id={data.id}>
+  let handleHover = (e) => {
+    setHovered(true)
+  }
+
+  let handleOffHover = (e) => {
+    setHovered(false)
+  }
+
+  return (
+
+  <div onMouseOver={ handleHover } onMouseOut={ handleOffHover } className={Style.item} id={data.id}>
     <div id={data.id} onClick={handleSelect} className={!available
         ? `${Style.item_card} ${Style.item_card_disabled}`
         : selected
@@ -56,8 +23,20 @@ const Item = ({data, available, handleSelect, selected}) => {
           : Style.item_card}>
 
       <div className={Style.item_text_wrapper}>
-        <span className={Style.item_text_slogan}>
-          {data.slogan}
+        <span className={
+          !selected
+            ? `${Style.item_text_slogan}`:
+          !isHovered
+            ? `${Style.item_text_slogan}`
+            : `${Style.item_text_slogan_angry}`
+          }>
+          {
+            !selected
+              ? data.slogan:
+            !isHovered
+              ? data.slogan
+              : data.angrySlogan
+          }
         </span>
         <span className={Style.item_text_title}>
           {data.title}
@@ -69,7 +48,7 @@ const Item = ({data, available, handleSelect, selected}) => {
           {data.offer}
         </span>
       </div>
-      
+
       <div className={Style.item_quant}>
         <span className={Style.item_quant_numb}>
           {data.weight}
@@ -78,7 +57,17 @@ const Item = ({data, available, handleSelect, selected}) => {
           {data.unit}
         </span>
       </div>
+
     </div>
+    <span className={!available ? Style.item_comment_sold : Style.item_comment}>
+      {
+        !available
+          ? data.soldOut :
+        !selected
+          ? data.callToAction
+          : data.tasteDetail
+      }
+    </span>
 
 </div>)
 }
